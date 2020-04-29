91d2591ef081e083f98827212fe27665ad11380d
GhAyoub <contact@ayoub-gharbi.org>
2020-04-29 16:57:50 +0100

    update window and entry point scripts

--- GAEngine/GAEngine/Window.cs
+++ GAEngine/GAEngine/Window.cs
@@ -5,51 +5,44 @@ using System;
 
 namespace GAEngine
 {
-    public class Window
+    public class GAWindow : GameWindow
     {
-        private readonly GameWindow _gameWindow;
-
-        public Window(string title, int width, int height)
+        public GAWindow() : base(900, 600, GraphicsMode.Default, "GAEngine")
         {
-            _gameWindow = new GameWindow(width, height, GraphicsMode.Default, title);
+            VSync = VSyncMode.On;
+            TargetRenderFrequency = 60;
+            TargetUpdateFrequency = 30;
         }
 
-        public void Start()
+        protected override void OnLoad(EventArgs e)
         {
-            _gameWindow.Resize += OnResize;
-            _gameWindow.Load += OnLoadWindow;
-            _gameWindow.RenderFrame += OnRender;
-            _gameWindow.Run(1.0 / 60.0);
+            GL.ClearColor(0f, 0f, 0f, 1f);
         }
 
-        // resize callback
-        private void OnResize(object sender, EventArgs e)
+        protected override void OnResize(EventArgs e)
         {
-            GL.Viewport(0, 0, _gameWindow.Width, _gameWindow.Height);
+            GL.Viewport(0, 0, Width, Height);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
             GL.Ortho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
         }
 
-        // frame render callback
-        private void OnRender(object sender, FrameEventArgs e)
+        protected override void OnRenderFrame(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            
+
             // draw triangle
             GL.Begin(PrimitiveType.Triangles);
             GL.Vertex2(1.0, 1.0);
             GL.Vertex2(25.0, 49.0);
             GL.Vertex2(49.0, 1.0);
             GL.End();
-            
-            _gameWindow.SwapBuffers();
+
+            SwapBuffers();
         }
 
-        // window's loaded callback
-        private void OnLoadWindow(object sender, EventArgs e)
+        protected override void OnUpdateFrame(FrameEventArgs e)
         {
-            GL.ClearColor(0f, 0f, 0f, 1f);
         }
     }
 }
