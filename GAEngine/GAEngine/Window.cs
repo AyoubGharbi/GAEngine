f872f3d5b8564568e27e6c51dfd597287327a66f
GhAyoub <contact@ayoub-gharbi.org>
2020-04-29 23:32:05 +0100

    Playing with blending and depth buffer

--- GAEngine/GAEngine/Window.cs
+++ GAEngine/GAEngine/Window.cs
@@ -2,12 +2,13 @@
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using System;
+using System.Drawing;
 
 namespace GAEngine
 {
     public class GAWindow : GameWindow
     {
-        public GAWindow() : base(900, 600, GraphicsMode.Default, "GAEngine")
+        public GAWindow() : base(900, 600, new GraphicsMode(32, 8, 0, 32), "GAEngine")
         {
             VSync = VSyncMode.On;
             TargetRenderFrequency = 60;
@@ -16,26 +17,40 @@ namespace GAEngine
 
         protected override void OnLoad(EventArgs e)
         {
-            GL.ClearColor(0f, 0f, 0f, 1f);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Lequal);
         }
 
         protected override void OnResize(EventArgs e)
         {
             GL.Viewport(0, 0, Width, Height);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Ortho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
+            //GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadIdentity();
+            //GL.Ortho(0.0, 50.0, 0.0, 50.0, -1.0, 1.0);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.ClearDepth(1);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             // draw triangle
             GL.Begin(PrimitiveType.Triangles);
-            GL.Vertex2(1.0, 1.0);
-            GL.Vertex2(25.0, 49.0);
-            GL.Vertex2(49.0, 1.0);
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0.5f);
+            GL.Color3(Color.Green);
+            GL.Vertex3(1, 0, 0.5f);
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 1, 0.5f);
+
+            GL.Color4(1f, 1f, 1f, .5f);
+            GL.Vertex3(-.25f, 1, .8f);
+            GL.Vertex3(1, -.25f, .8f);
+            GL.Vertex3(-.25f, -.25f, .8f);
             GL.End();
 
             SwapBuffers();
