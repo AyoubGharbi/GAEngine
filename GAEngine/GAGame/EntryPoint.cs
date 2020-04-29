2eff24ee7b04c18662529c36f52b60b6075af494
GhAyoub <contact@ayoub-gharbi.org>
2020-04-29 11:10:57 +0100

    initial commit of GAEngine

---
+++ GAEngine/GAGame/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using GAEngine;
+
+namespace GAGame
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Window gaWindow = new Window("GAEngine", 1200, 600);
+            gaWindow.Start();
+        }
+    }
+}
